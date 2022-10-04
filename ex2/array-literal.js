61156999aec614de32542d7be7d1e0f296ca7fe4
NguyenHoangHa9 <112064259+NguyenHoangHa9@users.noreply.github.com>
2022-10-04 23:15:02 +0700

    btvn js

---
+++ ex2/array-literal.js
@@ -0,0 +1,5 @@
+var colors;
+color = ['white', 'black', 'custom'];
+
+var el = document.getElementById('color');
+el.textContent = colors[0];
---
+++ ex2/array.js
@@ -0,0 +1,8 @@
+var color =['white', 'black', 'custom'];
+
+color[2] = 'beige';
+
+var el = document.getElement('colors');
+
+
+el.textContent = color[2];
